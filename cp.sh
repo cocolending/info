541af88fb95da959437a9580cd0fc1d66ccb8cf4
peekpi <894646171@qq.com>
2021-11-07 22:10:29 +0800

    x

--- cp.sh
+++ cp.sh
@@ -1,1 +1,5 @@
+rm -rf static
 cp -rf ../swap-info/build/* .
+git add .
+git commit -m "x"
+git push
---
+++ precache-manifest.db1232ebf561780a268079a5fc231800.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90515724d3fd22f296dba4f447d0306e",
+    "url": "info/index.html"
+  },
+  {
+    "revision": "f8327a1b219f25f00e2e",
+    "url": "info/static/js/2.922ab688.chunk.js"
+  },
+  {
+    "revision": "d255601c39ba9ea8be3f576f2fb7e1d5",
+    "url": "info/static/js/2.922ab688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7179cb9b5e084045b62",
+    "url": "info/static/js/main.9ed24783.chunk.js"
+  },
+  {
+    "revision": "faa531e255ecb37c9258",
+    "url": "info/static/js/runtime-main.3ddc2891.js"
+  },
+  {
+    "revision": "5827780d34d7c141d5e763fd7b596313",
+    "url": "info/static/media/logo.5827780d.svg"
+  },
+  {
+    "revision": "edb44e56409a7af705c227bd272fac47",
+    "url": "info/static/media/logo_white.edb44e56.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "info/static/media/wordmark_white.9914390f.svg"
+  }
+]);
